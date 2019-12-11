f5b6332e96eb6b07ba804a4a18b66b69369bc118
Rasmus <rasmusbamse@hotmail.com>
2019-12-11 17:20:58 +0100

    Rewrite build script.

--- build.py
+++ build.py
@@ -1,25 +1,37 @@
 import zipfile
-import sys
 import os
 
-def addDirToZip(zipHandle, path, basePath=""):
-	basePath = basePath.rstrip("\\/") + ""
-	basePath = basePath.rstrip("\\/")
+def addDirToZip(zipHandle, path):
 	for root, dirs, files in os.walk(path):
 		for file in files:
 			filePath = os.path.join(root, file)
-			inZipPath = filePath.replace(basePath, "", 1).lstrip("\\/")
+			inZipPath = filePath.replace(path, "", 1).lstrip("\\/")
 			zipHandle.write(filePath, inZipPath)
 
-def build_mod(modfolder, outputfile, steam = False):
-	if os.path.exists(outputfile):
-		os.remove(outputfile)
-	zipf = zipfile.ZipFile(outputfile, "w", zipfile.ZIP_DEFLATED)
-	addDirToZip(zipf, "common", "common")
-	addDirToZip(zipf, modfolder, modfolder)
-	zipf.close()
+class mod:
+	def __init__(self, modFolder, outputFile, steam = False):
+		self.modFolder = modFolder
+		self.outputFile = outputFile
+		self.steam = steam
 
-# ProMods
-build_mod("promods", "dmh_pm.scs")
-# ProMods w/ middle east addon
-build_mod("promods-mid", "dmh_pm_mid.scs")
+	def build(self):
+		if os.path.exists(self.outputFile):
+			os.remove(self.outputFile)
+		zipf = zipfile.ZipFile(self.outputFile, "w", zipfile.ZIP_DEFLATED)
+		addDirToZip(zipf, "common")
+		addDirToZip(zipf, self.modFolder)
+		zipf.close()
+
+modversions = [
+	# ProMods
+	mod("promods", "dmh_pm.scs"),
+	# ProMods w/ middle east addon
+	mod("promods-mid", "dmh_pm_mid.scs")
+]
+
+def main():
+	for modversion in modversions:
+		modversion.build()
+
+if __name__ == "__main__":
+	main()
